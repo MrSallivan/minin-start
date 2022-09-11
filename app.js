738bf2dfd444ebd6c7f5f9569fff5c5689918e7f
MrSallivan <nik.sallivan@gmail.com>
2022-09-11 19:35:04 +1000

    11.09

--- app.js
+++ app.js
@@ -2,22 +2,59 @@ const modal = document.querySelector('#modal')
 const content = document.querySelector('#content')
 const backdrop = document.querySelector('#backdrop')
 const progress = document.querySelector('#progress')
+const form = document.querySelector('#form')
 
 content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
+modal.addEventListener('change', toggleTech)
+form.addEventListener('submit', createTech)
 
-const technologies = [
-	{ title: 'HTML', description: 'HTML text', type: 'html', done: true },
-	{ title: 'CSS', description: 'CSS text', type: 'css', done: true},
-	{ title: 'JavaScript', description: 'JavaScript text', type: 'JavaScript', done: true },
-	{ title: 'React', description: 'React text', type: 'React', done: false },
-	{ title: 'GIT', description: 'GIT text', type: 'git', done: false}
-]
+const APP_TITLE = document.title
+const LS_KEY = 'MY_TECHS'
 
-function openCard() {
-	modal.classList.add('open')
+const technologies = getState()
+
+function openCard(event) {
+	const data = event.target.dataset
+	const tech = technologies.find(t => t.type === data.type)
+	if (!tech) return
+	openModal(toModal(tech), tech.title)
 }
+
+function toModal(tech) {
+	const checked = tech.done ? 'checked' : ''
+	return `
+		<h2>${tech.title}</h2>
+		<p>
+			${tech.description}
+		</p>
+		<hr />
+		<div>
+			<input type="checkbox" id="done" ${checked} data-type="${tech.type}"/>
+			<label for="done">Выучил</label>
+			<input type="checkbox" id="delete" ${checked} />
+			<label for="delete">Удалить</label>
+		</div>
+	`
+}
+
+function toggleTech(e) {
+	const type = e.target.dataset.type
+	const tech = technologies.find(t => t.type === type)
+	tech.done = e.target.checked
+	saveState()
+	init()
+}
+
+function openModal(html, title = APP_TITLE) {
+	document.title = title
+	modal.innerHTML = html
+	modal.classList.add('open')
+
+}
+
 function closeModal() {
+	document.title = APP_TITLE
 	modal.classList.remove('open')
 }
 
@@ -70,10 +107,54 @@ function toCard(tech) {
 	const doneClass = tech.done ? 'done' : ''
 
 	return `
-	<div class="card ${doneClass}">
-		<h3>${tech.title}</h3>
+	<div class="card ${doneClass}" data-type="${tech.type}">
+		<h3 data-type="${tech.type}">${tech.title}</h3>
 	</div>	
 	`
 }
+
+function isInvalid(title, description) {
+	return !title.value || !description.value
+}
+
+
+function createTech(e) {
+	e.preventDefault()
+	const { title, description } = e.target
+
+	if (isInvalid(title, description)) {
+		if (!title.value) title.classList.add('invalid')
+		if (!description.value) description.classList.add('invalid')
+
+		setTimeout(() => {
+			title.classList.remove('invalid')
+			description.classList.remove('invalid')
+		}, 2000)
+		return
+	}
+
+	const newTech = {
+		title: title.value,
+		description: description.value,
+		done: false,
+		type: title.value.toLowerCase()
+	}
+	technologies.push(newTech)
+	title.value = ''
+	description.value = ''
+	saveState()
+	init()
+}
+
+function saveState() {
+	localStorage.setItem(LS_KEY, JSON.stringify(technologies))
+}
+function getState() {
+	const raw = localStorage.getItem(LS_KEY)
+
+	return raw ? JSON.parse(raw) : []
+}
+
+
 
 init()
